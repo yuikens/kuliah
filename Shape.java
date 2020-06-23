76ef6d17bfb3dd0f7e0daf08a3dbf8d22040156f
yuikens <65400648+yuikens@users.noreply.github.com>
2020-06-23 09:52:18 +0700

    Update and rename Shape.php to Shape.java

--- Shape.java
+++ Shape.java
@@ -1,17 +1,17 @@
-package uas_2;
-import java.util.scanner;
+import java.util.Scanner;
 
-public class perulangan{
+public class Shape{
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		//Nomor 55
 		//Nama : Novia Shafa Hana Nabila
 		//NIM	: 191250855
 		int nim;
+
+		nim = input.nextInt();
 		
 		for(nim=9; nim<=55; nim++)	{
 			System.out.println(" " +nim);
 		}
-	}
 	}
 }
