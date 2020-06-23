ecd3ff774208af4e4e08f4aae4146c7b204dc6d2
yuikens <65400648+yuikens@users.noreply.github.com>
2020-06-23 09:51:45 +0700

    Create Shape.php

---
+++ Shape.php
@@ -0,0 +1,17 @@
+package uas_2;
+import java.util.scanner;
+
+public class perulangan{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		//Nomor 55
+		//Nama : Novia Shafa Hana Nabila
+		//NIM	: 191250855
+		int nim;
+		
+		for(nim=9; nim<=55; nim++)	{
+			System.out.println(" " +nim);
+		}
+	}
+	}
+}
